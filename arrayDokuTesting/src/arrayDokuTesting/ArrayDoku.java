4e87f81cbd5582e822364071db13bd1540a121c5
DarkChimu <darkchimu@hotmail.com>
2020-07-11 09:43:18 -0400

    ArrayDoku Testing Finalizado

---
+++ arrayDokuTesting/src/arrayDokuTesting/ArrayDoku.java
@@ -0,0 +1,14 @@
+package arrayDokuTesting;
+
+// Danyhover Bruzual CI: 26849550
+
+public class ArrayDoku {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		GUISudoku gs = new GUISudoku();
+        gs.setVisible(true);
+	}
+
+}
