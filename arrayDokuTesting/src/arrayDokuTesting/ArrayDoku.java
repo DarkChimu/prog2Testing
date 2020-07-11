3e132a242ee71e5324387721dc7600f1bf41e2fd
DarkChimu <darkchimu@hotmail.com>
2020-07-11 09:37:19 -0400

    ArrayDoku Finalizado

---
+++ arrayDokuTesting/src/arrayDokuTesting/ArrayDoku.java
@@ -0,0 +1,12 @@
+package arrayDokuTesting;
+
+public class ArrayDoku {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		GUISudoku gs = new GUISudoku();
+        gs.setVisible(true);
+	}
+
+}
---
+++ arrayDokuTesting/src/arrayDokuTesting/Save.java
@@ -0,0 +1,41 @@
+package arrayDokuTesting;
+import java.io.*;
+
+
+public class Save {
+    
+    File f;
+    BufferedReader br;
+    FileReader fr;
+    
+    public Save(){
+        File dir = new File(System.getenv("APPDATA")+"/.sudoku/saves");
+        dir.mkdirs();
+        this.f = new File (System.getenv("APPDATA")+"/.sudoku/saves/guardar");
+        try{
+            this.fr = new FileReader(f);
+            this.br = new BufferedReader(fr);
+        }catch(Exception e){
+            
+        }
+    }
+    
+    public void escribir(String texto){
+        try{
+            FileWriter w = new FileWriter(f);
+            BufferedWriter bw = new BufferedWriter(w);
+            PrintWriter pr = new PrintWriter(bw);
+            pr.write(texto);
+            pr.close();
+            bw.close();
+        }catch (Exception e){}
+    }
+    
+    public String leer(){
+        String linea="";
+        try{
+            linea = br.readLine();
+        }catch (Exception e){}
+        return linea;
+    }
+}
