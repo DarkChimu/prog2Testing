e9643138c9bf14f874be01a0f34e13faaf6528d0
DarkChimu <darkchimu@hotmail.com>
2020-07-11 09:48:28 -0400

    Mencion a la comunidad StackOverFlow

--- arrayDokuTesting/src/arrayDokuTesting/ArrayDoku.java
+++ arrayDokuTesting/src/arrayDokuTesting/ArrayDoku.java
@@ -2,6 +2,9 @@ package arrayDokuTesting;
 
 // Danyhover Bruzual CI: 26849550
 
+// ACLARACION: este testing ha sido posible gracias a la ayuda de la comunidad de StackOverflow.
+// Las soluciones expuestas no son propias al 100% debido al material de investigacion y consejos.
+
 public class ArrayDoku {
 
 	public static void main(String[] args) {
